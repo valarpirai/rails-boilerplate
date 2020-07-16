14aa8e1925251eebfb19c587b1f959bfa2d07d06
Valar <a.valarpirai@gmail.com>
2020-07-16 18:35:16 +0530

    Account Signup

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,11 @@
+class AccountsController < ApplicationController
+    def new
+        
+    end
+    
+    def signup
+        # Signup New account
+        # Signup Admin User
+        # redirect to User's signup complete
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root :to => 'home#index'
+  root to: 'home#index'
+
+  get '/accounts/new' => 'accounts#new'
+  post '/accounts/signup' => 'accounts#signup'
 
   devise_for :users
 
